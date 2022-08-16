290c8c9b0b91d6143e7baf3266a74b44cdb27270
Darshana2102 <109572859+Darshana2102@users.noreply.github.com>
2022-08-16 14:02:35 +0530

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public AppUser(string userName)
+        {
+            UserName = userName;
+        }
+
+        public int Id{ get; set;}
+        public string UserName { get; set;}
+
+    }
+}

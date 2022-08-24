e1faebff6f54b0c40f3a7d325d00810ce9d99323
Darshana2102 <109572859+Darshana2102@users.noreply.github.com>
2022-08-24 17:44:27 +0530

    End of Section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+// import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
